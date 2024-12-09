782cb51673d5dfc234792453559698ffbd3a3d0d
malay <malayranjan.s@basiscloudsolutions.com>
2024-12-09 13:11:03 +0530

    change var

--- Data/Anugal/Dev/clubcracker.py
+++ Data/Anugal/Dev/clubcracker.py
@@ -1,2 +1,4 @@
-clubcracker_url = "https://www.clubcrackers.com/en_US/home.html"
-clubcracker_browser = "Chrome"
+# clubcracker_url = "https://www.clubcrackers.com/en_US/home.html"
+# clubcracker_browser = "Chrome"
+clubcracker_url = "$clubcracker_url"
+clubcracker_browser = "$clubcracker_browser"
