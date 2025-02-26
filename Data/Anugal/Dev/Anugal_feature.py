838de439427d5b6fd4a37dd8e1ec0a65cc7e7a48
malay <malayranjan.s@basiscloudsolutions.com>
2025-02-26 16:55:42 +0530

    changes

--- Data/Anugal/Dev/Anugal_feature.py
+++ Data/Anugal/Dev/Anugal_feature.py
@@ -2,13 +2,16 @@ url ="https://dev.anugalid.com/login"
 Azure = "xpath://span[text()=' Azure']"
 browser = "Chrome"
 
-#### Anugal Login ######
-user_id = "malayranjan.s@basiscloudsolutions.com"
-password = "GzraO9MB"
+
+user_id = "$user_id"
+password = "$password"
 
 user_text_box = "id:emailId"
 password_text_box = "id:password"
 Login_anugal_button = "xpath://button[text()='Login']"
+Mail_id = "$Mail_id"
+Mail_password = "Mail_password"
+outlook = "$outlook"
 
 ENV = "BTP -APPS"
 
